d21af7b03cfe59a9f62aab7fa8015547a006cd1b
Iwahashi Aki <iwahashi@un-t.com>
2022-01-26 18:45:47 +0900

    画像差し替え

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-window.addEventListener('load', function () {
-  //プラグインを定義
-  gsap.registerPlugin(ScrollTrigger);
-
-  const sideTxt = document.querySelectorAll('.bl_side_heading');
-  gsap.to(sideTxt, {
-    x: 120,
-    duration: 0.8,
-    scrollTrigger: {
-      trigger: '.js-sideTxt-trigger',
-      start: 'bottom top',
-    },
-  });
-});
-
 (function ($) {
   $.scrollify({ 
     section: '.bl_area',
